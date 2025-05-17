b7246533fb7c0af436857b169a582d7b7b20d7ea
JRogerioSC <jo-rogerio@hotmail.com>
2025-05-17 19:59:58 -0300

    novo

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import  {PrismaClient}  from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+
+
+app.post('/usuarios', async (req,res) => {
+    
+    await prisma.user.create({
+        data:{
+         name: req.body.name,
+         menssage: req.body.menssage
+        }
+    })
+
+    res.status(201).json(req.body)
+
+})
+
+app.get('/usuarios',async (req,res) => {
+
+    const users = await prisma.user.findMany()
+
+    res.status(200).json(users)
+})
+
+app.listen(10000)
+
