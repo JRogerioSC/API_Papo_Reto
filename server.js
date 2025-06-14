383f3a15a97b30e16a861aa8aa0aae90a77dde5a
JRogerioSC <jo-rogerio@hotmail.com>
2025-06-13 23:46:25 -0300

    novod

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 app.use(cors({
-  origin: 'https://pap0reto.netlify.app/'
+  origin: 'https://pap0reto.netlify.app'
 }))
 
 
