37bf7019f0f2e60301ce6438fa3199ebe2a9b852
JRogerioSC <jo-rogerio@hotmail.com>
2025-06-13 23:45:10 -0300

    mais

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 app.use(cors({
-  origin: 'https://api-papo-reto.onrender.com'
+  origin: 'https://pap0reto.netlify.app/'
 }))
 
 
